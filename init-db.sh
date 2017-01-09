5e43a8a02ac097b4af508b7a5e89064e4fa7b571
benizar <benizar@gmail.com>
2017-01-09 18:28:50 +0100

    only compose db

--- init-db.sh
+++ init-db.sh
@@ -6,17 +6,11 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 
-echo "Creating testdb"
-"${psql[@]}" <<- 'EOSQL'
-CREATE DATABASE testdb;
-EOSQL
-
-
-echo "Load extensions in testdb and $POSTGRES_DB"
-#for DB in testdb "$POSTGRES_DB"; do
-for DB in testdb; do
+echo "Load extensions into $POSTGRES_DB"
+for DB in "$POSTGRES_DB"; do
 
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
+
 		CREATE EXTENSION IF NOT EXISTS pg_sakila_db;
 		CREATE EXTENSION IF NOT EXISTS pg_buffercache;
 		CREATE EXTENSION IF NOT EXISTS pg_adm;
